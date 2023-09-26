29e85525db240aa111d4bddd99a5131d64ce9d93
Piotr Michalak <128514914+piotrmichalak1@users.noreply.github.com>
2023-09-26 09:25:17 +0200

    FIT-1: Initialized Spring Boot REST API with /health endpoint.

---
+++ src/main/java/com/fittracker/fittracker/FitTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.fittracker.fittracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitTrackerApplication.class, args);
+	}
+
+}
