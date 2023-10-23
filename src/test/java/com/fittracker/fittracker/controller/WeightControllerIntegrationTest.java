eb3fab9e40a20cdbb54c7163d9a213cf65ab6eb7
Piotr Michalak <128514914+PiotrMichalak1@users.noreply.github.com>
2023-10-23 16:08:39 +0200

    Added post request Integration tests

--- src/test/java/com/fittracker/fittracker/controller/WeightControllerIntegrationTest.java
+++ src/test/java/com/fittracker/fittracker/controller/WeightControllerIntegrationTest.java
@@ -1,19 +1,40 @@
 package com.fittracker.fittracker.controller;
 
+import com.fittracker.fittracker.entity.Weight;
+import com.fittracker.fittracker.repository.WeightRepository;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.net.URI;
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @Testcontainers
+@AutoConfigureMockMvc
 public class WeightControllerIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    WeightRepository weightRepository;
 
     @Container
     @ServiceConnection
@@ -23,9 +44,14 @@ public class WeightControllerIntegrationTest {
             .withDatabaseName("testDatabase");
 
     @BeforeAll
-    static void setup(){
+    static void setUp() {
         System.setProperty("spring.config.name", "test-application");
         postgres.start();
+    }
+
+    @BeforeEach
+    void beforeSetUp() {
+        weightRepository.deleteAll();
     }
 
     @Test
@@ -34,5 +60,36 @@ public class WeightControllerIntegrationTest {
         assertEquals("testUser", postgres.getUsername());
         assertEquals("testPassword", postgres.getPassword());
         assertEquals("testDatabase", postgres.getDatabaseName());
+    }
+
+    @Test
+    void givenValidPostRequest_shouldSaveAnReturnWeight() throws Exception {
+        assertThat(weightRepository.findAll()).hasSize(0);
+
+        mockMvc.perform(post(URI.create("/api/v1/weight"))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"date\":\"2023-08-03\",\"value\":13.2}"))
+                .andExpect(status().isCreated())
+                .andExpect(content().string("{\"date\":\"2023-08-03\",\"value\":13.2}"));
+
+        var expected = new Weight(LocalDate.of(2023, 8, 3), 13.2);
+
+        var result = weightRepository.findAll();
+        assertThat(result).hasSize(1);
+        assertThat(result.iterator().next()).
+                usingRecursiveComparison().ignoringFields("id").isEqualTo(expected);
+
+    }
+    @Test
+    void givenValidPostRequestWithExistingDate_shouldReturnError() throws Exception {
+        assertThat(weightRepository.findAll()).hasSize(0);
+        weightRepository.save(new Weight(LocalDate.of(2023, 8, 3), 13.2));
+        mockMvc.perform(post(URI.create("/api/v1/weight"))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"date\":\"2023-08-03\",\"value\":13.2}"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string("{\"message\":\"Weight already exists for date: 2023-08-03\"}"));
+
+        assertThat(weightRepository.findAll()).hasSize(1);
     }
 }
