6a86bd231e15b44f64898b9ce37005ecbe06a411
Piotr Michalak <128514914+PiotrMichalak1@users.noreply.github.com>
2023-10-27 12:29:00 +0200

    Added dummy user and token valid till 2150 in WeightControllerIntegrationTest

--- src/test/java/com/fittracker/fittracker/controller/WeightControllerIntegrationTest.java
+++ src/test/java/com/fittracker/fittracker/controller/WeightControllerIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.fittracker.fittracker.controller;
 
+import com.fittracker.fittracker.entity.User;
 import com.fittracker.fittracker.entity.Weight;
+import com.fittracker.fittracker.repository.UserRepository;
 import com.fittracker.fittracker.repository.WeightRepository;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,6 +20,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.net.URI;
 import java.time.LocalDate;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -36,8 +39,12 @@ public class WeightControllerIntegrationTest {
 
     @Autowired
     WeightRepository weightRepository;
+    @Autowired
+    UserRepository userRepository;
 
     private final static String ENDPOINT = "/api/v1/weight";
+
+    private final static String TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c2VyIiwiaWF0IjoxNjk4NDAxMDA0LCJleHAiOjU3MDcwMTQ3NTR9.2D2rHl7L3Jpy1BxpZ8krRNFiGK0t6VGthS-9gQRVTOX9nM4QTj0m17X2RQspOnqTGaxY5rnCUBHOsyYWDaQ2cg";
 
     @ServiceConnection
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16");
@@ -45,11 +52,14 @@ public class WeightControllerIntegrationTest {
     @BeforeAll
     static void setUp() {
         postgres.start();
+
     }
 
     @BeforeEach
     void beforeSetUp() {
         weightRepository.deleteAll();
+        userRepository.deleteAll();
+        userRepository.save(new User(UUID.randomUUID(),"user","user@example.com","$2a$10$2gvLjc6wUEgM42M73tQ9ieI2jrAwfxap3X7XsEt//swQvJXyMpVJ6"));
     }
 
     @Nested
@@ -61,7 +71,8 @@ public class WeightControllerIntegrationTest {
 
             mockMvc.perform(post(URI.create(ENDPOINT))
                             .contentType(MediaType.APPLICATION_JSON)
-                            .content("{\"date\":\"2023-08-03\",\"value\":13.2}"))
+                            .content("{\"date\":\"2023-08-03\",\"value\":13.2}")
+                            .header("Authorization", "Bearer " + TOKEN))
                     .andExpect(status().isCreated())
                     .andExpect(content().string("{\"date\":\"2023-08-03\",\"value\":13.2}"));
 
@@ -80,7 +91,8 @@ public class WeightControllerIntegrationTest {
             weightRepository.save(new Weight(LocalDate.of(2023, 8, 3), 13.2));
             mockMvc.perform(post(URI.create(ENDPOINT))
                             .contentType(MediaType.APPLICATION_JSON)
-                            .content("{\"date\":\"2023-08-03\",\"value\":13.2}"))
+                            .content("{\"date\":\"2023-08-03\",\"value\":13.2}")
+                            .header("Authorization", "Bearer " + TOKEN))
                     .andExpect(status().isBadRequest())
                     .andExpect(content().string("{\"message\":\"Weight already exists for date: 2023-08-03\"}"));
 
@@ -96,7 +108,8 @@ public class WeightControllerIntegrationTest {
             weightRepository.save(new Weight(LocalDate.of(2023, 8, 3), 13.2));
             assertThat(weightRepository.findAll()).hasSize(1);
 
-            mockMvc.perform(get(URI.create(ENDPOINT + "?date=2023-08-03")))
+            mockMvc.perform(get(URI.create(ENDPOINT + "?date=2023-08-03"))
+                            .header("Authorization", "Bearer " + TOKEN))
                     .andExpect(status().isOk())
                     .andExpect(content().string("{\"date\":\"2023-08-03\",\"value\":13.2}"));
         }
@@ -105,7 +118,8 @@ public class WeightControllerIntegrationTest {
         void givenValidGetRequestWithNonExistingDate_shouldReturnError() throws Exception {
             assertThat(weightRepository.findAll()).hasSize(0);
 
-            mockMvc.perform(get(URI.create(ENDPOINT + "?date=2023-08-03")))
+            mockMvc.perform(get(URI.create(ENDPOINT + "?date=2023-08-03"))
+                            .header("Authorization", "Bearer " + TOKEN))
                     .andExpect(status().isNotFound())
                     .andExpect(content().string("{\"message\":\"Weight not found for date: 2023-08-03\"}"));
         }
